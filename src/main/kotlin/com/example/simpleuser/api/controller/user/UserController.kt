51f2ac7708f2e87c4d503349d9ed50c1671ded28
Artem Rynochnov <TemiX002@mail.ru>
2025-10-01 15:23:26 +0400

    Add .../me endpoint in UserController

--- src/main/kotlin/com/example/simpleuser/api/controller/user/UserController.kt
+++ src/main/kotlin/com/example/simpleuser/api/controller/user/UserController.kt
@@ -6,6 +6,8 @@ import com.example.simpleuser.service.user.UserService
 import com.example.simpleuser.store.entity.user.Role
 import org.springframework.http.HttpStatus
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -35,6 +37,11 @@ class UserController(
     @ResponseStatus(HttpStatus.OK)
     fun getByEmail(@PathVariable email: String): UserResponseDto =
         userService.getUserByEmail(email)
+
+    @GetMapping("/me")
+    @ResponseStatus(HttpStatus.OK)
+    fun getMe(@AuthenticationPrincipal currentUserDetails: UserDetails): UserResponseDto =
+        userService.getUserByEmail(currentUserDetails.username)
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
